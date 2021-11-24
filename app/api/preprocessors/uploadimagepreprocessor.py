22dc4645db159433a31ad61a8a1bbbc179dcb61e
Alexandre Leobons <aleobons@gmail.com>
2021-11-24 16:57:23 -0400

    Alteracao do retorno na leitura do arquivo (retorna somente o conteudo do arquivo) e verificacao de imagem valida (usando tf.io.is_jpeg)

--- app/api/preprocessors/uploadimagepreprocessor.py
+++ app/api/preprocessors/uploadimagepreprocessor.py
@@ -1,47 +1,48 @@
 import tensorflow as tf
 from fastapi import HTTPException
+import base64
 
 
 class UploadImagePreprocessor:
     """Classe que pré-processa os arquivos de imagens.
 
-    Possui apenas um método estático que lê o arquivo e converte em array
+    Possui apenas um método estático que lê o arquivo e converte em uma string decodificada
     """
 
     @staticmethod
     async def read_imagefile(upload_file):
         """Lê o arquivo de imagem.
 
-        Lê, valida e converte o arquivo de imagem em array
+        Lê, valida e converte o arquivo de imagem em uma string decodificada
 
         Args:
           upload_file: arquivo de imagem
 
         Returns:
-          Um array com a imagem
+          Uma string com a imagem decodificada
 
         Raises:
           HTTPException: Um erro devido ao upload de um arquivo que não é imagem
         """
 
         # primeiro verifica a extensão do arquivo, cuidando para aceitar extensão em caixa alta
-        extension_image = upload_file.filename.split(".")[-1].lower() in ("jpg", "jpeg", "png")
+        extension_image = upload_file.filename.split(".")[-1].lower() in ("jpg", "jpeg")
 
         # dispara um erro se a extensão não estiver na lista permitida
         if not extension_image:
             raise HTTPException(status_code=415, detail="Unsupported file provided.")
 
-        # lê o arquivo
-        file = await upload_file.read()
+        # lê o arquivo e codifica o arquivo
+        input_image = await upload_file.read()
+        # encoded_input_image_string = base64.b64encode(input_image)
 
-        # tenta abrir o arquivo como imagem e converte para array
-        try:
-            imagem = tf.image.decode_jpeg(file, channels=3)
-            imagem = imagem.numpy().tolist()
-
-        except tf.errors.InvalidArgumentError:
-            # se um erro de imagem não identificada for detectado, dispara um erro de arquivo inválido
+        # verifica se é uma imagem jpeg
+        if not tf.io.is_jpeg(input_image, name=None):
+            # caso negativo dispara um erro de arquivo inválido
             raise HTTPException(status_code=415, detail="Invalid file.")
 
-        # retorna a imagem em formato de Numpy array
-        return imagem
+        # input_image_string = encoded_input_image_string.decode("utf-8")
+
+        # retorna a imagem decodificada
+        # return input_image_string
+        return input_image
