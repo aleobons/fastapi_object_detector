f24763629b6d04c09a4b3ff42b8e47460fca77d2
aleobons@gmail.com <Gb1102bale>
2022-03-29 17:26:13 -0400

    nova versão do locust

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,27 +1,17 @@
-from locust import HttpUser, task, constant
-import json
-import tensorflow as tf
+from locust import HttpUser, task
 import os
 
 
 class LoadTest(HttpUser):
-    wait_time = constant(0)
     host = "http://localhost"
 
     @task
     def predict_placa_veiculo(self):
         image_path = "00011.jpg"
 
-        raw = tf.io.read_file(os.path.join('/mnt/locust/files', image_path))
-        image = tf.image.decode_jpeg(raw, channels=3)
-        image = image[tf.newaxis, ...]
-        image = image.numpy().tolist()
+        with open(os.path.join("files", image_path), "rb") as image:
+            data = [("images_file", image)]
 
-        # Create JSON to use in the request
-        data = {"instances": image}
+            url = "http://localhost:8401/detect_license_plate/crop"
 
-        headers = {"content-type": "application/json"}
-
-        url = 'http://detector_placa_veiculos:8501/v1/models/detector_placa_veiculos/labels/stable:predict'
-
-        self.client.request("POST", url, data=json.dumps(data), headers=headers)
+            self.client.request("POST", url, files=data)
